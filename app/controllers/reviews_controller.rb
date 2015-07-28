b1931677bbd70e6a4dc556b46b92863553304064
Paul Aglione <aglionep@gmail.com>
2015-07-28 15:52:16 -0700

    private methods in reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,19 @@
 class ReviewsController < ApplicationController
+	def index
+	end
+
+
+	private 
+	def review_params
+		params.require(:review).permit(:comments, :rating)
+	end
+
+	def get_field
+		Field.find(params[:field_id])
+	end
+
+	def get_review
+		get_field.reviews.find(params[:id])
+	end
 end
+
